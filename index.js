a6b32601c5b2078bedb7feb3ec932c08cb503951
Ayush-3012 <diveshsrivastava31@gmail.com>
2023-08-05 00:01:57 +0530

    add new pages, transfer of data from one file to another using urls

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 import express from "express";
 import bodyParser from "body-parser";
+import questions from "./questions.js";
+import _ from "lodash";
 
 const app = express();
 const port = 3000;
 
-app.use(express.static("public"))
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   res.render("index.ejs");
+});
+
+app.get("/about", async (req, res) => {
+  res.render("about.ejs");
+});
+
+app.get("/:topic", async (req, res) => {
+  const requestedDs = _.lowerCase(req.params.topic);
+  questions.forEach(function (item) {
+    if (_.lowerCase(item.topicName) == requestedDs)
+      res.render("sheet.ejs", {
+        dataStructure: item,
+      });
+  });
 });
 
 // app.post("/submit", (req, res) => {
