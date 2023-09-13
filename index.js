ac53f7ae76857f95ee80ffe54fd6c97e714e4610
Ayush-3012 <diveshsrivastava31@gmail.com>
2023-09-13 20:52:04 +0530

    vercel deployment try -5

--- index.js
+++ index.js
@@ -5,11 +5,10 @@ import _ from "lodash";
 import mongoose from "mongoose";
 
 const app = express();
-const port = 3000;
+const port = 3000 || `process.env.PORT`;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
-// mongoose.connect("mongodb://127.0.0.1:27017/dsaTrackerDB");
 mongoose.connect(
   "mongodb+srv://Ayush-3012:Champ%403012@cluster0.veabqcp.mongodb.net/dsaTrackerDB"
 );
