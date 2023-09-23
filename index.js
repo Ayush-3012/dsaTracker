cdea2f6d9c0c120fd7608f563f6386f4bfe84fec
Ayush-3012 <diveshsrivastava31@gmail.com>
2023-09-23 15:43:18 +0530

    to find views and its files 5

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ import mongoose from "mongoose";
 const app = express();
 const port = 3000;
 
-// app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
-// app.use(express.static(__dirname + "public"));
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
